e88ee3aea92965e0d664202e66d3b59ff96a78d4
Steven <solanors20@gmail.com>
2020-03-28 18:20:20 -0600

    -V funcion added - Single Step

--- system_calls_tracker.c
+++ system_calls_tracker.c
@@ -8,93 +8,70 @@
 #include "sys_call_table/sys_call_table.h"
 #include "sys_call_table/sys_call_table.c"
 #include <sys/signal.h>
+#include <stdio.h>
 
 int main()
 {
-    debug_without_step();
+    debug_with_step();
     return 0;
 }
 
 void debug_with_step(){
-    pid_t child;
-    long orig_eax, eax;
-    long params[3];
-    int status;
-    int insyscall = 0;
-    int Tpid, stat, res;
-    int signo = 0;
+ pid_t child;
+    const int long_size = sizeof(long);
     child = fork();
-    if (child == 0)
-    {
+    if(child == 0) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
         execl("/bin/ls", "ls", NULL);
     }
-    else
-    {
+    else {
         int status;
-        wait(&status);
+        union u {
+            long val;
+            char chars[long_size];
+        }data;
         struct user_regs_struct regs;
-        res = waitpid(child, &stat, WUNTRACED);
-        if ((res != child) || !(WIFSTOPPED(stat)) ) {
-            printf("Unexpected wait result res %d stat %x\n",res,stat);
-            exit(1);
-        }
-        while (1)
-        {
-            ptrace(PTRACE_GETREGS, child, NULL, &regs);
-            printf("eip: %x\n", (unsigned int) regs.rcx);
-            if ((res = ptrace(PTRACE_SINGLESTEP, child, 0, signo)) < 0) {
-			perror("Ptrace singlestep error");
-			exit(1);
-		}
-		res = wait(&stat);
-        if ((signo = WSTOPSIG(stat)) == SIGTRAP) {
-			signo = 0;
-		}
-        if ((signo == SIGHUP) || (signo == SIGINT)) {
-			ptrace(PTRACE_CONT, child, 0, signo);
-			printf("Child took a SIGHUP or SIGINT. We are done\n");
-			break;
-		}
-            //waitpid(child, &status, 0);
-            if(WIFEXITED(status)) break;
-            /*
-		    res = wait(&stat);
-            if (WIFEXITED(stat))
+        int start = 0;
+        long ins;
+        char ch;
+        while(1) {
+            wait(&status);
+            if(WIFEXITED(status))
                 break;
-            orig_eax = ptrace(PTRACE_PEEKUSER,
-                              child, 8 * ORIG_RAX, NULL);
-
-            if (insyscall == 0)
-            {
-                /* Syscall entry */
-                /*insyscall = 1;
-                params[0] = ptrace(PTRACE_PEEKUSER,
-                                   child, 8 * RBX,
-                                   NULL);
-                params[1] = ptrace(PTRACE_PEEKUSER,
-                                   child, 8 * RCX,
-                                   NULL);
-                params[2] = ptrace(PTRACE_PEEKUSER,
-                                   child, 8 * RDX,
-                                   NULL);
-                char *syscall = get_sys_call_name(orig_eax);
+            ptrace(PTRACE_GETREGS,
+                   child, NULL, &regs);
+       
+                ins = ptrace(PTRACE_PEEKTEXT,
+                             child, regs.rip,
+                             NULL);
+                char *syscall = get_sys_call_name(regs.orig_rax);
                 printf("System call: %s     RBX: %ld    RCX: %ld    RDX: %ld\n",
                        syscall,
-                       params[0],
-                       params[1],
-                       params[2]);
-            }
-            else
-            { 
-                eax = ptrace(PTRACE_PEEKUSER,
-                             child, 8 * ORIG_RAX, NULL);
-                insyscall = 0;
-            }
-            ptrace(PTRACE_SYSCALL,
-                   child, NULL, NULL);*/
+                       regs.rbx,
+                       regs.rcx,
+                       regs.rdx);
+            
+          
+                ptrace(PTRACE_SINGLESTEP, child,
+                       NULL, NULL);
+          
+                
+                //printf("Enter any character: ");
+                //read a single character
+                ch=fgetc(stdin);
+                
+                if(ch==0x0A)
+                {
+                    ptrace(PTRACE_SYSCALL, child,
+                       NULL, NULL);
+                }
+                //read dummy character to clear
+                //input buffer, which inserts after character input
+             
+            
         }
     }
+    return 0;
 }
 
 void debug_without_step(){
