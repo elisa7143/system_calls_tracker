171d9ccc3e86a2778414a0f0ade3e94544fc9fc3
Steven <solanors20@gmail.com>
2020-05-01 19:39:25 -0600

    Total argument validation

--- system_calls_tracker.c
+++ system_calls_tracker.c
@@ -9,8 +9,119 @@
 #include "sys_call_table/sys_call_table.c"
 #include <sys/signal.h>
 #include <stdio.h>
+#include <sys/mman.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <errno.h>
 
-int main()
+
+/*
+ * Check if a file exist using fopen() function
+ * return 1 if the file exist otherwise return 0
+*/
+
+//This method validate if the program the user is trying to run exists!
+int ifcommandexists(const char * fname){
+    if (system(("which %s > /dev/null 2>&1\n", fname))) {
+        // Command doesn't exist...
+        exit(EXIT_FAILURE);
+    } else {
+        // Command does exist, do something with it...
+        printf("------------------------------------------------\n");
+        printf("The program introduced exists\n");
+        printf("------------------------------------------------\n");
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int opt, val, i;
+    char *program;
+    char *pseudo_program;
+    i = 0;
+    if(argc > 6){
+        printf("Expected less than 5 arguments.\n");
+        exit(EXIT_FAILURE);
+    }
+    //these lines of code are placed to validate if there are any parameters
+    if (optind >= argc){
+        printf("argc %d", argc);
+        fprintf(stderr, "Expected argument after main %s [-v or -V program name]\n"
+                        "ex. ./main -v soffice -writer\n"
+                        "    ./main -V soffice -draw\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    //This two lines of code validate if the program exists
+    pseudo_program = strcat(strdup(argv[2]), " --help");
+    ifcommandexists(pseudo_program);
+
+    char * args0[2] = {"",NULL};
+    char * args1[3] = {"","", NULL};
+    char * args2[4] = {"", "", "", NULL};
+    char * args3[5] = {"", "", "", "", NULL};
+    //These lines of code permit to run programs that need one or many parameters
+    while (argv[i] != NULL){
+        if (i == 2){
+            program =  strdup(argv[i]);
+        } else if (i > 2) {
+            program = strcat(program, " ");
+            program = strcat(program, strdup(argv[i]));
+        }
+        if (i == 2){
+            args0[0] =  strdup(argv[i]);
+            args1[0] =  strdup(argv[i]);
+            args2[0] =  strdup(argv[i]);
+            args3[0] =  strdup(argv[i]);
+        } else if (i > 2) {
+            args0[i-2] =  strdup(argv[i]);
+            args1[i-2] =  strdup(argv[i]);
+            args2[i-2] =  strdup(argv[i]);
+            args3[i-2] =  strdup(argv[i]);
+        }
+        i++;
+    }
+
+    printf("Program to be run: %s \n", program);
+    printf("------------------------------------------------\n");
+
+    char * path = "/bin/ls";
+    char * args = "ls";
+    //These lines of code are to select between the two possible options.
+    while ((opt = getopt(argc, argv, "if:vV")) != -1)
+    {
+        switch (opt){
+            case 'v':
+                if(argc == 3){
+                    debug_without_step(args0[0],args0);
+                }else if(argc == 4){
+                    debug_without_step(args0[0],args1);
+                }else if(argc == 5){
+                    debug_without_step(args0[0],args2);
+                }else if(argc == 6){
+                    debug_without_step(args0[0],args3);
+                }
+                _exit(EXIT_SUCCESS);
+            case 'V':
+                if(argc == 3){
+                    debug_with_step(args0[0],args0);
+                }else if(argc == 4){
+                    debug_with_step(args0[0],args1);
+                }else if(argc == 5){
+                    debug_with_step(args0[0],args2);
+                }else if(argc == 6){
+                    debug_with_step(args0[0],args3);
+                }
+                exit(EXIT_SUCCESS);
+            default:
+                fprintf(stderr, "Usage: %s [-v or -V program name]\n",
+                        argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+}
+
+/*int main()
 {
     char * path = "/bin/ls";
     char * args = "ls";
@@ -24,15 +135,15 @@ int main()
     }
     
     return 0;
-}
+}*/
 
-void debug_with_step(char * path,char * args){
- pid_t child;
+void debug_with_step(char * path,char * args[]){
+    pid_t child;
     const int long_size = sizeof(long);
     child = fork();
     if(child == 0) {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-        execl(path, args, NULL);
+        execv(path, args);
     }
     else {
         int status;
@@ -84,7 +195,7 @@ void debug_with_step(char * path,char * args){
     return 0;
 }
 
-void debug_without_step(char * path,char * args){
+void debug_without_step(char * path,char * args[]){
     pid_t child;
     long orig_eax, eax;
     long params[3];
@@ -94,7 +205,7 @@ void debug_without_step(char * path,char * args){
     if (child == 0)
     {
         ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-        execl(path, args, NULL);
+        execv(path, args);
     }
     else
     {
